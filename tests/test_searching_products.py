bbe2c23221e98f30e12190f9e7f5c4263f176c78
Mika Lackman <mika.lackman(at)gmail.com>
2013-07-11 22:56:08 +0300

    xapian usage added

--- tests/test_searching_products.py
+++ tests/test_searching_products.py
@@ -1,22 +1,47 @@
 from os.path import join, pardir
 import sys
+import shutil
 import unittest
 
 sys.path.insert(0, join(pardir))
-from prodcomp import ProductCompare
+from prodcomp import Product, ProductRepository
 
 test_database_path = 'testdb'
 
+class AcceptanceTestBase(unittest.TestCase):
 
-class NoProductsAcceptanceTest(unittest.TestCase):
+    def setUp(self):
+        shutil.rmtree(test_database_path, ignore_errors=True)
+        self.repository = ProductRepository(test_database_path)
+
+class NoProductsAcceptanceTest(AcceptanceTestBase):
+
 
     def testItShouldReturnEmptyResult(self):
-
-        pc = ProductCompare(test_database_path)
-        result = pc.search('no such product')
+        result = self.repository.search('no such product')
 
         self.assertEquals(result.page_count, 0)
         self.assertEquals(result.products, [])
+
+class OneProductAcceptanceTest(AcceptanceTestBase):
+
+    def testItShouldReturnMatchingProduct(self):
+        expectedProduct = Product(url='product url', \
+                                  title='prodict title', \
+                                  price='10.00', \
+                                  description='product desc', \
+                                  image_url='product image url')
+
+        self.repository.add_product(expectedProduct)
+
+        result = self.repository.search('product')
+
+        self.assertEquals(result.page_count, 1)
+        self.assertEquals(len(result.products), 1)
+    
+        self.assertEquals(result.products[0], expectedProduct)
+
+
 
 if __name__ == '__main__':
     unittest.main()
